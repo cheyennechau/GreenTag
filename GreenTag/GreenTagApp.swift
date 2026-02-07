4e2b79b83306e1a32f3f89a119c05843b441d5e8
Cheyenne Chau <ciaobellacheyenne@gmail.com>
2026-02-07 12:26:00 -0800

    Initial Commit

---
+++ GreenTag/GreenTagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenTagApp.swift
+//  GreenTag
+//
+//  Created by Cheyenne Chau on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct GreenTagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
